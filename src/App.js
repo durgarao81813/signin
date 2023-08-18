b7f190ff61d4651b1c3bf82c23882902d8019f22
durgarao <durgarao81813@gmail.com>
2023-08-18 10:31:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Content from './Content.js'
 import Signin from './Signin.js'
+import Welcome from './Welcome.js'
 import './App.css';
 
 const App=()=>(
@@ -10,6 +11,8 @@ const App=()=>(
       <Routes>
         <Route  excat path="/" element={<Content/>}/>
         <Route  excat path="/signin" element={<Signin/>}/>
+        <Route  excat path="/welcome" element={<Welcome/>}/>
+        
       </Routes>
   </BrowserRouter>
 )
--- src/Content.js
+++ src/Content.js
@@ -1,81 +1,80 @@
 
 import './App.css';
-import Signin from './Signin';
-import {FiHome} from 'react-icons/fi'
-import {FcGoogle} from 'react-icons/fc'
-import {AiOutlineApple}from 'react-icons/ai'
+
+import { FiHome } from 'react-icons/fi'
+import { FcGoogle } from 'react-icons/fc'
+import { AiOutlineApple } from 'react-icons/ai'
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import {AiOutlineArrowRight} from 'react-icons/ai';
+import {useNavigate} from 'react-router-dom'
+
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 
 function Content() {
-  return (  
-    <div className="container-fluid main">
-      <div className='row'>
-        <div className='col-md-4  bg-dark first'>
-          
-          
+    const navigate=useNavigate()
+    const data=()=>{
+        navigate('/signin')
+    }
+    
+    return (
+        <div className="container-fluid main">
+            <div className='row'>
+                <div className='col-md-4  bg-dark first'>
+
+
+
+                </div>
+                <div className='col-md-8'>
+                    <div className='row'>
+                        <div className='col-12 d-flex justify-content-end mt-3'>
+                            <FiHome className='iconsize' />
+                        </div>
+                    </div>
+                    <div className='row connect'>
+                        <form onSubmit={data}>
+                            <div className='col-12 '>
+                                <h1>Sign up</h1>
+                                <p>Completely Empolyee-owened, we're driven with a passion</p>
+                            </div>
+                            <div className='col-12 col-lg-6 mt-3 ' >
+                                <div className='d-flex iconbutton justify-content-center'>
+                                    <FcGoogle className='iconsize2' />
+                                    <p className='m-1'>continue with Google</p>
+                                </div>
+                            </div>
+                            <div className='col-12 col-lg-6 mt-3' >
+                                <div className='d-flex iconbutton justify-content-center'>
+                                    <AiOutlineApple className='iconsize2' />
+                                    <p className='m-1'>continue with Apple</p>
+                                </div>
+                            </div>
+                            <div align="center" className='mt-4'>
+                                <h5>Or</h5>
+                            </div>
+                            <div className='col-12'>
+                                
+                                    <TextField id="standard-basic"  className="inputtext" label="Enter Your Username or Email *" variant="standard" required />
+                                    <br/>
+                                    <TextField id="standard-basic" className="inputtext" label="Enter Your Password *" type="Password" variant="standard" required />
+                                    <br/>
+                                    <TextField id="standard-basic" className="inputtext" label="Confirm  Password *" type="Password" variant="standard" required />
+                                
+                                <br />
+                                <a href="target">Forget Password  </a>
+                                <div className='d-flex justify-content-end mt-5'>
+                                    <button type="button" className='btn' >Sign in</button>
+                                    <button type="submit" className='btn btn-primary'>Continue <AiOutlineArrowRight /></button>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
-        <div className='col-md-8'>
-          <div className='row'>
-            <div className='col-12 d-flex justify-content-end mt-3'>
-              <FiHome className='iconsize'/>
-            </div>
-          </div>
-          <div className='row connect'>
-           <form>
-            <div className='col-12 '>
-              <h1>Sign up</h1>
-              <p>Completely Empolyee-owened, we're driven with a passion</p>
-            </div>
-            <div className='col-12 col-lg-6 mt-3 ' >
-              <div className='d-flex iconbutton justify-content-center'>
-              <FcGoogle className='iconsize2'/>
-              <p className='m-1'>continue with Google</p>
-              </div>
-            </div>  
-            <div className='col-12 col-lg-6 mt-3' >
-              <div className='d-flex iconbutton justify-content-center'>
-                <AiOutlineApple className='iconsize2'/>
-                <p className='m-1'>continue with Apple</p>
-              </div>
-            </div>
-            <div align="center" className='mt-4'>
-              <h5>Or</h5>
-            </div>
-            <div  className='col-12'>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '100%' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField id="standard-basic" label="Enter Your Username or Email *"  variant="standard"  required />
-                <TextField id="standard-basic" label="Enter Your Password *" type="Password" variant="standard" required/>
-                <TextField id="standard-basic" label="Confirm  Password *" type="Password" variant="standard" required/>
-            </Box>
-            <br/>
-            <a href="target">Forget Password</a>
-            <div className='d-flex justify-content-end mt-5'>
-                            <button type="button" className='btn' >Sign in</button>
-                            <button type="submit" className='btn btn-primary'>Continue <AiOutlineArrowRight/></button>
-
-            </div>
-
-
-            </div>
-            </form>
-          </div>
-        </div>
-
-      </div>
-      
-    </div>
-  );
+    );
 }
 
 export default Content;
